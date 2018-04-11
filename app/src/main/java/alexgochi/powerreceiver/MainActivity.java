d4298249b8e6c56333a0ac6f2a7dc3feb38150f7
Alex Gochi <alex.gochi.javan@gmail.com>
2018-04-11 19:59:14 +0700

    Initial commit

---
+++ app/src/main/java/alexgochi/powerreceiver/MainActivity.java
@@ -0,0 +1,57 @@
+package alexgochi.powerreceiver;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String ACTION_CUSTOM_BROADCAST = "alexgochi.powerreceiver.ACTION_CUSTOM_BROADCAST";
+
+    private ComponentName mReceiverComponentName;
+    private PackageManager mPackageManager;
+    private CustomReceiver mReceiver = new CustomReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mReceiverComponentName = new ComponentName(this, CustomReceiver.class);
+        mPackageManager = getPackageManager();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
+    }
+
+    @Override
+    protected void onStart() {
+        mPackageManager.setComponentEnabledSetting
+                (mReceiverComponentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mPackageManager.setComponentEnabledSetting
+                (mReceiverComponentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
+    }
+}
